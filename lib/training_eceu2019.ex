59bdae7f37ed93308fa2af1a7d916065aac9f799
Andrea Leopardi <an.leopardi@gmail.com>
2019-04-10 13:38:17 +0200

    Refactor get_address/0

--- lib/training_eceu2019.ex
+++ lib/training_eceu2019.ex
@@ -1,16 +1,18 @@
 defmodule ChatClient do
   def start() do
+    {hostname, port} = get_address()
+    IO.inspect({hostname, port})
+  end
+
+  defp get_address() do
     address = String.trim(IO.gets("Server address (localhost:4000): "))
 
-    {hostname, port} =
-      if address == "" do
-        {"localhost", 4000}
-      else
-        [hostname, port] = String.split(address, ":")
-        port = String.to_integer(port)
-        {hostname, port}
-      end
-
-    IO.inspect({hostname, port})
+    if address == "" do
+      {"localhost", 4000}
+    else
+      [hostname, port] = String.split(address, ":")
+      port = String.to_integer(port)
+      {hostname, port}
+    end
   end
 end
