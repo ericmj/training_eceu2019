6dc068964c98a287fc58a597d46341e1b30fbd51
Andrea Leopardi <an.leopardi@gmail.com>
2019-04-10 11:19:01 +0200

    Next step

--- lib/training_eceu2019.ex
+++ lib/training_eceu2019.ex
@@ -1,6 +1,16 @@
 defmodule ChatClient do
   def start() do
-    address = String.trim(IO.gets("Server address: "))
-    IO.inspect(address)
+    address = String.trim(IO.gets("Server address (localhost:4000): "))
+
+    {hostname, port} =
+      if address == "" do
+        {"localhost", 4000}
+      else
+        [hostname, port] = String.split(address, ":")
+        port = String.to_integer(port)
+        {hostname, port}
+      end
+
+    IO.inspect({hostname, port})
   end
 end
