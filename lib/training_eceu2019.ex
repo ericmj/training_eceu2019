5d0865b9dc8e5a0609da2749dda4739ea9d93d74
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2019-04-10 14:10:20 +0200

    Add TCP connect and loop

--- lib/training_eceu2019.ex
+++ lib/training_eceu2019.ex
@@ -1,18 +1,33 @@
 defmodule ChatClient do
   def start() do
     {hostname, port} = get_address()
-    IO.inspect({hostname, port})
+    {:ok, socket} = :gen_tcp.connect(hostname, port, mode: :binary, active: true)
+
+    loop(socket)
+  end
+
+  defp loop(socket) do
+    receive do
+      {:tcp, ^socket, data} ->
+        IO.inspect(data)
+        loop(socket)
+
+      {:tcp_closed, ^socket} ->
+        raise "TCP connection was closed"
+
+      {:tcp_error, ^socket, reason} ->
+        raise "TCP connection error: #{:inet.format_error(reason)}"
+    end
   end
 
   defp get_address() do
     address = String.trim(IO.gets("Server address (localhost:4000): "))
 
     if address == "" do
-      {"localhost", 4000}
+      {'localhost', 4000}
     else
       [hostname, port] = String.split(address, ":")
-      port = String.to_integer(port)
-      {hostname, port}
+      {String.to_charlist(hostname), String.to_integer(port)}
     end
   end
 end
